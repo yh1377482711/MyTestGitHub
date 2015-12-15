2f104f9eb9a93ed1f83c4c828a70060e28026706
Yuhan <yu.han@onemeter.co>
2015-12-15 11:08:58 +0800

    添加了onDestroy()方法

--- app/src/main/java/onemeter/com/mytestgithub/MainActivity.java
+++ app/src/main/java/onemeter/com/mytestgithub/MainActivity.java
@@ -1,5 +1,6 @@
 package onemeter.com.mytestgithub;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
     @Override
     protected void onResume() {
